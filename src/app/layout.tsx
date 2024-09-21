053084b5977e332084c8283ae3704ab1ab9ec8bd
preska <wolfwar@outlook.com>
2024-09-22 00:37:14 +0200

    ini

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import localFont from 'next/font/local';
+
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+
+import "./app.css"
+
+ 
+const alpino = localFont({
+  src: '../../public/fonts/Alpino-Variable.woff2',
+  weight:"100 900",
+  display: 'swap',
+  variable:"--font-alpino"
+})
+ 
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={alpino.variable}>
+      <body>{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
